57ae8f6c0d118d8f8cf97928923635b3414fdd15
dan <dan@ack.me>
2017-06-22 21:52:10 -0700

    added atmel ASF drivers

---
+++ atmel/ASF/ASF.mk
@@ -0,0 +1,34 @@
+NAME := atmel_mcu_drivers
+
+
+GLOBAL_INCLUDES := . \
+                    common/boards \
+                    common/services/clock \
+                    common/services/ioport \
+                    common/utils \
+                    sam/drivers/adc \
+                    sam/drivers/crccu \
+                    sam/drivers/dacc \
+                    sam/drivers/efc \
+                    sam/drivers/gpbr \
+                    sam/drivers/matrix \
+                    sam/drivers/pdc \
+                    sam/drivers/pio \
+                    sam/drivers/pmc \
+                    sam/drivers/pwm \
+                    sam/drivers/rstc \
+                    sam/drivers/rtc \
+                    sam/drivers/rtt \
+                    sam/drivers/spi \
+                    sam/drivers/supc \
+                    sam/drivers/tc \
+                    sam/drivers/twi \
+                    sam/drivers/uart \
+                    sam/drivers/usart \
+                    sam/drivers/wdt \
+                    sam/utils \
+                    sam/utils/cmsis/sam4s/include \
+                    sam/utils/cmsis/sam4s/source/templates \
+                    sam/utils/header_files \
+                    sam/utils/preprocessor \
+                    thirdparty/CMSIS/Include
